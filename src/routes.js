5af9358d580addbfca9cacdc53482df142ac0866
PraDika24 <handukaagus108@gmail.com>
2024-12-05 17:25:35 +0800

    add allData Handler

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+// USER HANDLER
 const {
     register,
     login,
@@ -9,32 +10,47 @@ const {
     getUsers,
   } = require("./handler/user");
 
+
+// DATABASE HANDLER
+const { getAll, deleteAll } = require("./handler/allData");
+
 const routes = [
+    //LOGIN
     {
         method: "POST",
         path: "/login",
         handler: login,
     },
+
+    // REGISTER
     {
         method: "POST",
         path: "/register",
         handler: register,
       },
+
+      // Get All User
       {
         method: "GET",
         path: "/users",
         handler: getAllUsers,
-      }, 
+      },
+      
+      // Get Data User by Id
       {
         method: "GET",
         path: "/users/{id}",
         handler: getUsers,
       },
+
+      //RESET PASSWORD
       {
         method: "POST",
         path: "/resetPassword",
         handler: resetPassword,
       },
+
+      //EDIT USER PROFILE
       {
         method: "PUT",
         path: "/users/{id}",
@@ -60,6 +76,27 @@ const routes = [
         method: "DELETE",
         path: "/users",
         handler: deleteAllUserData,
+      },
+
+
+      // ----------------------
+      // Endpoint /database
+      // ----------------------
+
+      // GET /database
+      // all - Ambil Seluruh Data Database
+      {
+        method: "GET",
+        path: "/database",
+        handler: getAll,
+      },
+
+      //  DELETE /database
+      // all - Hapus Seluruh Data Database
+      {
+        method: "DELETE",
+        path: "/database",
+        handler: deleteAll,
       },
     
 
